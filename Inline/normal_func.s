efbee08e4a664eda0749a1e5bdbd75f6912ab186
Shubham Varne <89720812+ShubhamVarne@users.noreply.github.com>
2023-11-14 17:25:04 +0530

    Add files via upload

---
+++ Inline/normal_func.s
@@ -0,0 +1,56 @@
+	.file	"normal_func.c"
+	.text
+	.globl	_sum
+	.def	_sum;	.scl	2;	.type	32;	.endef
+_sum:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%eax, %edx
+	movl	16(%ebp), %eax
+	movl	%edx, (%eax)
+	nop
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	___main
+	movl	$100, 28(%esp)
+	movl	$200, 24(%esp)
+	movl	$0, 20(%esp)
+	leal	20(%esp), %eax
+	movl	%eax, 8(%esp)
+	movl	24(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	28(%esp), %eax
+	movl	%eax, (%esp)
+	call	_sum
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE1:
+	.ident	"GCC: (i686-posix-dwarf-rev0, Built by MinGW-W64 project) 8.1.0"
